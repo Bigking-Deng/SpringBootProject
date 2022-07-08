dc1f37ff49d70ceeded2156e1c162ec860acbab3
Bigking-Deng <61732769+Bigking-Deng@users.noreply.github.com>
2022-07-08 16:36:22 +0800

    Update gitexec.java

--- src/main/java/exercise/gitexec.java
+++ src/main/java/exercise/gitexec.java
@@ -6,5 +6,6 @@ public class gitexec {
     public static void main(String[] args) {
         System.out.println("sds");
         System.out.println("revert test");
+        System.out.println("zhangsan add‘);
     }
 }
